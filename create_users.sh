986d1fe708a0b8a5aca9fe90d9a5cb3aa810afdb
Carsten Bock <carsten@ng-voice.com>
2018-03-21 15:32:04 +0100

    Add opc in impi, add any visited network

--- create_users.sh
+++ create_users.sh
@@ -29,17 +29,17 @@ do
 	echo "START TRANSACTION;" >> ./create.sql
 	echo "insert into imsu (name, id_capabilities_set, id_preferred_scscf_set) values ('loadtest-$i', 1, 1);" >> ./create.sql
 	echo "set @imsu_id=last_insert_id();" >> ./create.sql
-	echo "INSERT INTO impi (id_imsu, identity, k, auth_scheme, default_auth_scheme, amf, op, sqn, ip, line_identifier, zh_uicc_type, zh_key_life_time, zh_default_auth_scheme) VALUES (@imsu_id, 'contract$i', '$i-$PASSWORD-$i', 255, 4, '', '', '000000000000', '', '', 0, 3600, 1);" >> ./create.sql
+	echo "INSERT INTO impi (id_imsu, identity, k, auth_scheme, default_auth_scheme, amf, op, opc, sqn, ip, line_identifier, zh_uicc_type, zh_key_life_time, zh_default_auth_scheme) VALUES (@imsu_id, 'contract$i', '$i-$PASSWORD-$i', 255, 4, '', '', '', '000000000000', '', '', 0, 3600, 1);" >> ./create.sql
 	echo "set @impi1_id=last_insert_id();" >> ./create.sql
-	echo "INSERT INTO impi (id_imsu, identity, k, auth_scheme, default_auth_scheme, amf, op, sqn, ip, line_identifier, zh_uicc_type, zh_key_life_time, zh_default_auth_scheme) VALUES (@imsu_id, '$PREFIX$i@$DOMAIN', '$i-password-$i', 255, 4, '', '', '000000000000', '', '', 0, 3600, 1);" >> ./create.sql
+	echo "INSERT INTO impi (id_imsu, identity, k, auth_scheme, default_auth_scheme, amf, op, opc, sqn, ip, line_identifier, zh_uicc_type, zh_key_life_time, zh_default_auth_scheme) VALUES (@imsu_id, '$PREFIX$i@$DOMAIN', '$i-password-$i', 255, 4, '', '', '', '000000000000', '', '', 0, 3600, 1);" >> ./create.sql
 	echo "set @impi2_id=last_insert_id();" >> ./create.sql
 	echo "INSERT INTO impu (identity, type, barring, user_state, id_sp, id_implicit_set, id_charging_info, wildcard_psi, display_name, psi_activation, can_register) VALUES('sip:$PREFIX$i@$DOMAIN', 0, 0, 1, 1, 0, 1, '', '', 0, 1);" >> ./create.sql
 	echo "set @impu1_id=last_insert_id();" >> ./create.sql
-	echo "INSERT INTO impu_visited_network (id_impu, id_visited_network) VALUES(@impu1_id, 6);" >> ./create.sql
+	echo "INSERT INTO impu_visited_network (id_impu, id_visited_network) SELECT @impu1_id, id FROM visited_network;" >> ./create.sql
 	echo "update impu set id_implicit_set=@impu1_id where id=@impu1_id;" >> ./create.sql
 	echo "INSERT INTO impu (identity, type, barring, user_state, id_sp, id_implicit_set, id_charging_info, wildcard_psi, display_name, psi_activation, can_register) VALUES('tel:$PREFIX$i', 0, 0, 1, 1, @impu1_id, 1, '', '', 0, 1);" >> ./create.sql
 	echo "set @impu2_id=last_insert_id();" >> ./create.sql
-	echo "INSERT INTO impu_visited_network (id_impu, id_visited_network) VALUES(@impu2_id, 6);" >> ./create.sql
+	echo "INSERT INTO impu_visited_network (id_impu, id_visited_network) SELECT @impu2_id, id FROM visited_network;" >> ./create.sql
 	echo "INSERT INTO impi_impu (id_impi, id_impu) VALUES(@impi1_id, @impu1_id);" >> ./create.sql
 	echo "INSERT INTO impi_impu (id_impi, id_impu) VALUES(@impi1_id, @impu2_id);" >> ./create.sql
 	echo "INSERT INTO impi_impu (id_impi, id_impu) VALUES(@impi2_id, @impu1_id);" >> ./create.sql
